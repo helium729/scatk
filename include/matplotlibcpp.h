4c4a38fdc430d2f871db1981b2b259883561d42b
Xuanyu Hu <helium729@126.com>
2023-06-18 00:41:30 +0800

    add windows support

--- include/matplotlibcpp.h
+++ include/matplotlibcpp.h
@@ -351,10 +351,13 @@ template <> struct select_npy_type<uint64_t> { const static NPY_TYPES type = NPY
 
 // Sanity checks; comment them out or change the numpy type below if you're compiling on
 // a platform where they don't apply
+// if mingw compiler is used, then NPY_INT64 and NPY_UINT64 are not defined
+#ifndef __MINGW32__
 static_assert(sizeof(long long) == 8);
 template <> struct select_npy_type<long long> { const static NPY_TYPES type = NPY_INT64; };
 static_assert(sizeof(unsigned long long) == 8);
 template <> struct select_npy_type<unsigned long long> { const static NPY_TYPES type = NPY_UINT64; };
+#endif // __MINGW32__
 
 template<typename Numeric>
 PyObject* get_array(const std::vector<Numeric>& v)
